a2e371af14c606ca1e674b202cdd7f6b93d296a3
Ben Brunton <ben@flashtalking.com>
2014-08-04 10:06:09 +0100

    removed target from repo

--- src/video.rs
+++ src/video.rs
@@ -3,7 +3,35 @@ use sdl2;
 pub fn main() {
     sdl2::init(sdl2::InitVideo);
 
-    let window = match sdl2::video::Window::new("rust-sdl2 demo: Video", sdl2::video::PosCentered, sdl2::video::PosCentered, 800, 600, sdl2::video::OpenGL) {
+    let title = "rusteroids";
+    let window_x_pos = sdl2::video::PosCentered;
+    let window_y_pos = sdl2::video::PosCentered;
+    let window_width = 800;
+    let window_height = 600;
+    /*  Flags
+        Fullscreen              |   SDL_WINDOW_FULLSCREEN           |   fullscreen window
+        OpenGL                  |   SDL_WINDOW_OPENGL               |   window usable with OpenGL context
+        Shown                   |   SDL_WINDOW_SHOWN                |   window is visible
+        Hidden                  |   SDL_WINDOW_HIDDEN               |   window is not visible
+        Borderless              |   SDL_WINDOW_BORDERLESS           |   no window decoration
+        Resizable               |   SDL_WINDOW_RESIZABLE            |   window can be resized
+        Minimized               |   SDL_WINDOW_MINIMIZED            |   window is minimized
+        Maximized               |   SDL_WINDOW_MAXIMIZED            |   window is maximized
+        InputGrabbed            |   SDL_WINDOW_INPUT_GRABBED        |   window has grabbed input focus
+        InputFocus              |   SDL_WINDOW_INPUT_FOCUS          |   window has input focus
+        MouseFocus              |   SDL_WINDOW_MOUSE_FOCUS          |   window has mouse focus
+        FullscreenDesktop       |   SDL_WINDOW_FULLSCREEN_DESKTOP   |   fullscreen window
+        Foreign                 |   SDL_WINDOW_FOREIGN              |   window not created by SDL
+        SDL_WINDOW_OPENGL
+        
+        OR'd together
+        e.g:
+
+        sdl2::video::OpenGL | sdl2::video::Borderless
+     */
+    let window_flags = /*sdl2::video::Fullscreen | */ sdl2::video::OpenGL | sdl2::video::Borderless;
+
+    let window = match sdl2::video::Window::new(title, window_x_pos, window_y_pos, window_width, window_height, window_flags) {
         Ok(window) => window,
         Err(err) => fail!(format!("failed to create window: {}", err))
     };
