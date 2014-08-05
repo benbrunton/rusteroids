41b301da3cb5ce9cb8d643845ce10a2b4ec88055
Ben Brunton <ben@flashtalking.com>
2014-08-05 09:53:08 +0100

    switched to glfw and opengl

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,192 @@
 #![feature(globs)]
 
-extern crate sdl2;
+extern crate gl;
+extern crate glfw;
 extern crate native;
 
-mod video;
-mod actor;
-//mod gl2;
+use gl::types::*;
+use glfw::Context;
+use std::mem;
+use std::ptr;
+use std::str;
+
+// Vertex data
+static VERTEX_DATA: [GLfloat, ..6] = [
+     0.0,  0.05,
+     0.025, -0.05,
+    -0.025, -0.05
+];
+
+// Shader sources
+static VS_SRC: &'static str =
+   "#version 150\n\
+    in vec2 position;\n\
+    void main() {\n\
+       gl_Position = vec4(position, 0.0, 1.0);\n\
+    }";
+
+static FS_SRC: &'static str =
+   "#version 150\n\
+    out vec4 out_color;\n\
+    void main() {\n\
+       out_color = vec4(1.0, 0.3, 0.3, 0.8);\n\
+    }";
+
+
+fn compile_shader(src: &str, ty: GLenum) -> GLuint {
+    let shader = gl::CreateShader(ty);
+    unsafe {
+        // Attempt to compile the shader
+        src.with_c_str(|ptr| gl::ShaderSource(shader, 1, &ptr, ptr::null()));
+        gl::CompileShader(shader);
+
+        // Get the compile status
+        let mut status = gl::FALSE as GLint;
+        gl::GetShaderiv(shader, gl::COMPILE_STATUS, &mut status);
+
+        // Fail on error
+        if status != (gl::TRUE as GLint) {
+            let mut len = 0;
+            gl::GetShaderiv(shader, gl::INFO_LOG_LENGTH, &mut len);
+            let mut buf = Vec::from_elem(len as uint - 1, 0u8);     // subtract 1 to skip the trailing null character
+            gl::GetShaderInfoLog(shader, len, ptr::mut_null(), buf.as_mut_ptr() as *mut GLchar);
+            fail!("{}", str::from_utf8(buf.as_slice()).expect("ShaderInfoLog not valid utf8"));
+        }
+    }
+    shader
+}
+
+fn link_program(vs: GLuint, fs: GLuint) -> GLuint {
+    let program = gl::CreateProgram();
+    gl::AttachShader(program, vs);
+    gl::AttachShader(program, fs);
+    gl::LinkProgram(program);
+    unsafe {
+        // Get the link status
+        let mut status = gl::FALSE as GLint;
+        gl::GetProgramiv(program, gl::LINK_STATUS, &mut status);
+
+        // Fail on error
+        if status != (gl::TRUE as GLint) {
+            let mut len: GLint = 0;
+            gl::GetProgramiv(program, gl::INFO_LOG_LENGTH, &mut len);
+            let mut buf = Vec::from_elem(len as uint - 1, 0u8);     // subtract 1 to skip the trailing null character
+            gl::GetProgramInfoLog(program, len, ptr::mut_null(), buf.as_mut_ptr() as *mut GLchar);
+            fail!("{}", str::from_utf8(buf.as_slice()).expect("ProgramInfoLog not valid utf8"));
+        }
+    }
+    program
+}
 
 fn main() {
+    let glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
 
-    video::main();
+    // Choose a GL profile that is compatible with OS X 10.7+
+    glfw.window_hint(glfw::ContextVersion(3, 2));
+    glfw.window_hint(glfw::OpenglForwardCompat(true));
+    glfw.window_hint(glfw::OpenglProfile(glfw::OpenGlCoreProfile));
+
+    let (window, events) = glfw.create_window(800, 600, "rusteroids", glfw::Windowed)
+        .expect("Failed to create GLFW window.");
+
+    window.set_all_polling(true);
+
+    // It is essential to make the context current before calling `gl::load_with`.
+    window.make_current();
+
+    // Load the OpenGL function pointers
+    gl::load_with(|s| glfw.get_proc_address(s));
+
+    // Create GLSL shaders
+    let vs = compile_shader(VS_SRC, gl::VERTEX_SHADER);
+    let fs = compile_shader(FS_SRC, gl::FRAGMENT_SHADER);
+    let program = link_program(vs, fs);
+
+    let mut vao = 0;
+    let mut vbo = 0;
+
+    unsafe {
+        // Create Vertex Array Object
+        gl::GenVertexArrays(1, &mut vao);
+        gl::BindVertexArray(vao);
+
+        // Create a Vertex Buffer Object and copy the vertex data to it
+        gl::GenBuffers(1, &mut vbo);
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+        gl::BufferData(gl::ARRAY_BUFFER,
+                       (VERTEX_DATA.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+                       mem::transmute(&VERTEX_DATA[0]),
+                       gl::STATIC_DRAW);
+
+        // Use shader program
+        gl::UseProgram(program);
+        "out_color".with_c_str(|ptr| gl::BindFragDataLocation(program, 0, ptr));
+
+        // Specify the layout of the vertex data
+        let pos_attr = "position".with_c_str(|ptr| gl::GetAttribLocation(program, ptr));
+        gl::EnableVertexAttribArray(pos_attr as GLuint);
+        gl::VertexAttribPointer(pos_attr as GLuint, 2, gl::FLOAT,
+                                gl::FALSE as GLboolean, 0, ptr::null());
+    }
+
+    while !window.should_close() {
+        // Poll events
+        glfw.poll_events();
+
+        // Clear the screen to black
+        gl::ClearColor(0.2, 0.2, 0.4, 1.0);
+        gl::Clear(gl::COLOR_BUFFER_BIT);
+
+        // Draw a triangle from the 3 vertices
+        gl::DrawArrays(gl::TRIANGLES, 0, 3);
+
+        // Swap buffers
+        window.swap_buffers();
+
+        for event in glfw::flush_messages(&events) {
+            handle_window_event(&window, event);
+        }
+    }
+
+    // Cleanup
+    gl::DeleteProgram(program);
+    gl::DeleteShader(fs);
+    gl::DeleteShader(vs);
+    unsafe {
+        gl::DeleteBuffers(1, &vbo);
+        gl::DeleteVertexArrays(1, &vao);
+    }
+}
+
+fn handle_window_event(window: &glfw::Window, (time, event): (f64, glfw::WindowEvent)) {
+    match event {
+        glfw::PosEvent(x, y)                => window.set_title(format!("Time: {}, Window pos: ({}, {})", time, x, y).as_slice()),
+        glfw::SizeEvent(w, h)               => window.set_title(format!("Time: {}, Window size: ({}, {})", time, w, h).as_slice()),
+        glfw::CloseEvent                    => println!("Time: {}, Window close requested.", time),
+        glfw::RefreshEvent                  => println!("Time: {}, Window refresh callback triggered.", time),
+        glfw::FocusEvent(true)              => println!("Time: {}, Window focus gained.", time),
+        glfw::FocusEvent(false)             => println!("Time: {}, Window focus lost.", time),
+        glfw::IconifyEvent(true)            => println!("Time: {}, Window was minimised", time),
+        glfw::IconifyEvent(false)           => println!("Time: {}, Window was maximised.", time),
+        glfw::FramebufferSizeEvent(w, h)    => println!("Time: {}, Framebuffer size: ({}, {})", time, w, h),
+        glfw::CharEvent(character)          => println!("Time: {}, Character: {}", time, character),
+        glfw::MouseButtonEvent(btn, action, mods) => println!("Time: {}, Button: {}, Action: {}, Modifiers: [{}]", time, glfw::ShowAliases(btn), action, mods),
+        glfw::CursorPosEvent(xpos, ypos)    => window.set_title(format!("Time: {}, Cursor position: ({}, {})", time, xpos, ypos).as_slice()),
+        glfw::CursorEnterEvent(true)        => println!("Time: {}, Cursor entered window.", time),
+        glfw::CursorEnterEvent(false)       => println!("Time: {}, Cursor left window.", time),
+        glfw::ScrollEvent(x, y)             => window.set_title(format!("Time: {}, Scroll offset: ({}, {})", time, x, y).as_slice()),
+        glfw::KeyEvent(key, scancode, action, mods) => {
+            println!("Time: {}, Key: {}, ScanCode: {}, Action: {}, Modifiers: [{}]", time, key, scancode, action, mods);
+            match (key, action) {
+                (glfw::KeyEscape, glfw::Press) => window.set_should_close(true),
+                (glfw::KeyR, glfw::Press) => {
+                    // Resize should cause the window to "refresh"
+                    let (window_width, window_height) = window.get_size();
+                    window.set_size(window_width + 1, window_height);
+                    window.set_size(window_width, window_height);
+                }
+                _ => {}
+            }
+        }
+    }
 }
