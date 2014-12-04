1113f44d0a175fcb42cd71623cd0ac623ccc238f
Ben Brunton <benbrunton@hotmail.com>
2014-12-04 10:28:03 +0000

    fixed for rust nightly

--- src/explosion.rs
+++ src/explosion.rs
@@ -97,7 +97,7 @@ impl Actor for Explosion{
             rotation: self.rotation * PI / 180.0,
             shape: self.shape.clone(),
             color: self.color.clone(),
-            collision_type: actor::Ignore,
+            collision_type: actor::CollisionType::Ignore,
             show_secondary: false,
             secondary_shape: None,
             secondary_color: None,
